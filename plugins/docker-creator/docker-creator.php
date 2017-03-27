24a2ed255fc2c3ce487a53c145c6f5e0e39e7620
Rob Landers <landers.robert@gmail.com>
2017-03-26 23:58:18 -0400

    Refactored to class

--- plugins/docker-creator/docker-creator.php
+++ plugins/docker-creator/docker-creator.php
@@ -19,14 +19,136 @@ require_once( 'vendor/autoload.php' );
 use Docker\Docker;
 use Docker\DockerClient;
 
-function BaseDockerFile() {
-	$wpVersion = getenv( 'WORDPRESS_VERSION' );
+class DockerDeployment {
+	function __construct() {
+	}
 
-	return [
-		'contents' => "
+	function BaseFile() {
+		$wpVersion = getenv( 'WORDPRESS_VERSION' );
+
+		return [
+			'contents' => "
 FROM withinboredom/scalable-wordpress:$wpVersion-apache
 "
-	];
+		];
+	}
+
+	function getUrlFromSlug( $plugin ) {
+		$url = null;
+		switch ( $plugin['type'] ) {
+			case 'wporg':
+				$slug     = $plugin['slug'] ?: die( 'invalid slug' );
+				$version  = $plugin['version'] ?: 'latest';
+				$response = wp_remote_get( "https://api.wordpress.org/plugins/info/1.0/$slug.json" );
+				$response = json_decode( $response['body'] );
+
+				if ( $response === null ) {
+					die( 'invalid slug' );
+				}
+
+				$url = $response->download_link;
+
+				if ( $version !== 'latest' ) {
+					$url = explode( '.', $url )[0] . ".$version" . '.zip';
+				}
+
+				break;
+			case 'url':
+				$url = $plugin['url'];
+				break;
+		}
+
+		return $url;
+	}
+
+	function BuildAndPush( WP_REST_Request $params ) {
+		$key = wp_generate_password( 12, false, false );
+		set_transient( 'docker_' . $key, $params, DAY_IN_SECONDS );
+		$repo = $params->get_param( 'repo' );
+		//update_option( 'docker_' . $key, serialize( $params ) );
+		$client = new DockerClient( [
+			'remote_socket' => 'unix:///var/run/docker.sock'
+		] );
+
+		$docker = new Docker( $client );
+
+		$tar        = new PharData( ABSPATH . "/wp-content/$key.tar" );
+		$dockerfile = $this->Extend( $params )['contents'];
+
+		$tar->addFromString( 'Dockerfile', $dockerfile );
+		unset( $tar );
+
+		$build = [
+			'dockerfile' => 'Dockerfile',
+			't'          => "$repo:$key",
+			'pull'       => true
+		];
+
+		$tar     = ABSPATH . "/wp-content/$key.tar";
+		$tarball = file_get_contents( $tar );
+		unlink( $tar );
+
+		try {
+			$results = $docker->getImageManager()->build( $tarball, $build );
+			unset( $tarball );
+			$output = [];
+			foreach ( $results as $result ) {
+				$output[] = $result->getStream();
+			}
+
+			$results = $docker->getImageManager()->push( "$repo:$key", [
+				'X-Registry-Auth' => base64_encode( json_encode( [
+					'username' => $params->get_param( 'username' ),
+					'password' => $params->get_param( 'password' ),
+					'email'    => $params->get_param( 'email' )
+				] ) )
+			] );
+			foreach ( $results as $result ) {
+				$output[] = $result->getProgress() . ' :: ' . $result->getStatus();
+			}
+
+			return [
+				'output' => $output,
+				'image'  => "$repo:$key"
+			];
+		} catch ( Exception $exception ) {
+			return [
+				$exception->getMessage(),
+				$exception->getTrace()
+			];
+		}
+	}
+
+	function Extend( WP_REST_Request $params ) {
+		$plugins = $params->get_param( 'plugins' );
+		$theme   = $params->get_param( 'themes' );
+
+		$base = $this->BaseFile()['contents'];
+
+		$base .= "USER www-data\n";
+
+		foreach ( $plugins as $plugin ) {
+			$url = $this->getUrlFromSlug( $plugin );
+
+			if ( empty( $url ) ) {
+				continue;
+			}
+
+			$base .= "RUN curl $url > /var/www/html/wp-content/plugins/$(basename $url) && cd /var/www/html/wp-content/plugins && unzip $(basename $url)\n";
+		}
+		unset( $plugin );
+
+		$themeUrl = $this->getUrlFromSlug( $theme );
+		if ( ! empty( $themeUrl ) ) {
+			$base .= "RUN curl $themeUrl > /var/www/html/wp-content/themes/$(basename $themeUrl) && cd /var/www/html/wp-content/themes && unzip \n";
+		}
+
+		$base .= "USER root\n";
+
+		return [
+			"contents" => $base
+		];
+	}
 }
 
 /*
@@ -51,155 +173,19 @@ Expects json of the form:
 }
 */
 
-function getDockerSlugUrl( $plugin ) {
-	$url = null;
-	switch ( $plugin['type'] ) {
-		case 'wporg':
-			$slug     = $plugin['slug'] ?: die( 'invalid slug' );
-			$version  = $plugin['version'] ?: 'latest';
-			$response = wp_remote_get( "https://api.wordpress.org/plugins/info/1.0/$slug.json" );
-			$response = json_decode( $response['body'] );
-
-			if ( $response === null ) {
-				die( 'invalid slug' );
-			}
-
-			$url = $response->download_link;
-
-			if ( $version !== 'latest' ) {
-				$url = explode( '.', $url )[0] . ".$version" . '.zip';
-			}
-
-			break;
-		case 'url':
-			$url = $plugin['url'];
-			break;
-	}
-
-	return $url;
-}
-
-/**
- * @param WP_REST_Request $params
- *
- * @return array
- */
-
-function ExtendDockerFile( WP_REST_Request $params ) {
-	$plugins = $params->get_param( 'plugins' );
-	$theme   = $params->get_param( 'themes' );
-
-	$base = BaseDockerFile()['contents'];
-
-	$base .= "USER www-data\n";
-
-	foreach ( $plugins as $plugin ) {
-		$url = getDockerSlugUrl( $plugin );
-
-		if ( empty( $url ) ) {
-			continue;
-		}
-
-		$base .= "RUN curl $url > /var/www/html/wp-content/plugins/$(basename $url) && cd /var/www/html/wp-content/plugins && unzip $(basename $url)\n";
-	}
-	unset( $plugin );
-
-	$themeUrl = getDockerSlugUrl( $theme );
-	if ( ! empty( $themeUrl ) ) {
-		$base .= "RUN curl $themeUrl > /var/www/html/wp-content/themes/$(basename $themeUrl) && cd /var/www/html/wp-content/themes && unzip \n";
-	}
-
-	$base .= "USER root\n";
-
-	return [
-		"contents" => $base
-	];
-}
-
-function BuildDockerWP( WP_REST_Request $params ) {
-	$key = wp_generate_password( 12, false, false );
-	set_transient( 'docker_' . $key, $params, DAY_IN_SECONDS );
-	$repo = $params->get_param( 'repo' );
-	//update_option( 'docker_' . $key, serialize( $params ) );
-	$client = new DockerClient( [
-		'remote_socket' => 'unix:///var/run/docker.sock'
-	] );
-
-	$docker = new Docker( $client );
-
-	$tar        = new PharData( ABSPATH . "/wp-content/$key.tar" );
-	$dockerfile = ExtendDockerFile( $params )['contents'];
-
-	$tar->addFromString( 'Dockerfile', $dockerfile );
-	unset( $tar );
-
-	$build = [
-		'dockerfile' => 'Dockerfile',
-		't'          => "$repo:$key",
-		'pull'       => true
-	];
-
-	$tar     = ABSPATH . "/wp-content/$key.tar";
-	$tarball = file_get_contents( $tar );
-	unlink( $tar );
-
-	try {
-		$results = $docker->getImageManager()->build( $tarball, $build );
-		unset( $tarball );
-		$output = [];
-		foreach ( $results as $result ) {
-			$output[] = $result->getStream();
-		}
-
-		$results = $docker->getImageManager()->push( "$repo:$key", [
-			'X-Registry-Auth' => base64_encode( json_encode( [
-				'username' => $params->get_param( 'username' ),
-				'password' => $params->get_param( 'password' ),
-				'email'    => $params->get_param( 'email' )
-			] ) )
-		] );
-		foreach ( $results as $result ) {
-			$output[] = $result->getProgress() . ' :: ' . $result->getStatus();
-		}
-
-		
-
-		return [
-			'output' => $output,
-			'image'  => "$repo:$key"
-		];
-	} catch ( Exception $exception ) {
-		return [
-			$exception->getMessage(),
-			$exception->getTrace()
-		];
-	}
-}
+$GLOBALS['DOCKER'] = new DockerDeployment();
 
 add_action( 'rest_api_init', function () {
 	register_rest_route( 'docker/v1', 'file', [
 		'methods'  => 'GET',
-		'callback' => 'BaseDockerFile'
+		'callback' => [ $GLOBALS['DOCKER'], 'BaseFile' ]
 	] );
 	register_rest_route( 'docker/v1', 'file', [
 		'methods'  => 'POST',
-		'callback' => 'ExtendDockerFile'
-	] );
-	register_rest_route( 'docker/v1', 'Dockerfile', [
-		'methods'  => 'GET',
-		'callback' => function ( $request ) {
-			//$request = unserialize( get_option( 'docker_' . $request->get_param( 'key' ) ) );
-			$request = get_transient( 'docker_' . $request->get_param( 'key' ) );
-
-			$contents = ExtendDockerFile( $request )['contents'];
-
-			update_option( 'docker_' . $request->get_param( 'key' ), null );
-
-			return $contents;
-		}
+		'callback' => [ $GLOBALS['DOCKER'], 'Extend' ]
 	] );
 	register_rest_route( 'docker/v1', 'build', [
 		'methods'  => 'POST',
-		'callback' => 'BuildDockerWP'
+		'callback' => [ $GLOBALS['DOCKER'], 'BuildAndPush' ]
 	] );
 } );
